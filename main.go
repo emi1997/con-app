15fc655f99f1743ca14976ec9f6707251d107c61
Julia Kettl <keju@WEDV.Abensberg>
2019-09-09 15:05:34 +0200

    Initializing App

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"github.com/spf13/cobra"
+	"./cmd"
 )
 
 func main()  {
+	cmd.Execute()
 	fmt.Println("When I´m grown upp, I´ll be an application for your console!")
 }
