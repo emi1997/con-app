9205f19d8db26cfb50e9883d121cacf4bf86d56d
Julia Kettl <keju@WEDV.Abensberg>
2019-09-09 10:29:57 +0200

    Added append function

--- general/code/go/slice.go
+++ general/code/go/slice.go
@@ -23,8 +23,12 @@ func main(){
 	//b[:2] == []string{"g", "o"}
 	fmt.Println(letters, length, capacity, b[1:4], slice)
 	//Increase the cpacity of a slice:
-	//1. make a bigger slice
+	//1. make a bigger slice and copy the data from the previous slice
 	t := make([]byte, len(s), (cap(s)+1)*2)//+1 in case s == 0
 	copy(t, s)
 	fmt.Println(t, s)
+	//2. Append data to the end of a slice:
+	a := make([]int, 1)
+	a = append(a, 1, 2, 3, 4, 5)
+	fmt.Println(a)
 }
