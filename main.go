82c7fdb7b44e01cb8cd5e27d2fd877798eb7a95f
Julia Kettl <keju@WEDV.Abensberg>
2019-09-10 09:10:17 +0200

    Back to the beginning

--- main.go
+++ main.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/spf13/cobra"
-	"./cmd"
 )
 
 func main()  {
-	cmd.Execute()
 	fmt.Println("When I´m grown upp, I´ll be an application for your console!")
 }
