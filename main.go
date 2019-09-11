f0313527b25eccdbbd2d1a63d80c7e8a0b8a08e0
Julia Kettl <keju@WEDV.Abensberg>
2019-09-11 12:06:13 +0200

    update

--- main.go
+++ main.go
@@ -1,18 +1,16 @@
 package main
 
 import (
-	"log"
-	"github.com/elastic/go-elasticsearch/v7"
+	// "log"
+	// "context"
+	"github.com/olivere/elastic"
 )
 
 func main(){
-	cfg := elasticsearch.Config{
-		Addresses: []string{
-		  "http://localhost:9200",
-		  "http://localhost:9201",
-		},
-	  }
-	elastic, _ := elasticsearch.NewClient(cfg)
-	log.Println(elasticsearch.Version)
-	log.Println(elastic.Info())
+	client, err := elastic.NewClient()
+	if err != nil {
+	// Handle error
+	panic(err)
+	}
+	defer client.Stop()
 }
