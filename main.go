5f61de52904f304e2d2b3e74c2550fb9825aad6a
Julia Kettl <keju@WEDV.Abensberg>
2019-09-11 13:09:39 +0200

    update

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	// "log"
 	// "context"
+	"net/http"
 	"github.com/olivere/elastic"
 )
 
 func main(){
-	client, err := elastic.NewClient()
+	//Creating a new client for Elasticsearch instance
+	client, err := elastic.NewClient(elastic.SetURL("http://localhost:9200"))
 	if err != nil {
 	// Handle error
 	panic(err)
